cc787d91a42a14c9897b4193b8bd59cd8b95f1b8
efrain2010 <efrain.villanueva3@gmail.com>
2020-05-31 23:33:24 +0100

    iOS app with just Hello World

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Efraín Villanueva Castilla on 31/05/20.
+//  Copyright © 2020 Efraín Villanueva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# My first iOS app, just a Hello Word
